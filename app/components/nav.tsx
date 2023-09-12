2e578c5838c523734c325e882b4a4bb2355334c8
ratidzo <ratygore@gmail.com>
2023-09-12 12:03:31 +0200

    feat: setup links
    Setup product & sales links on nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,14 @@
-import {Josefin_Sans} from 'next/font/google'
+import {Josefin_Sans, Inria_Sans} from 'next/font/google'
+import Link from 'next/link';
 
 const josefin_sans = Josefin_Sans({
     weight: '700',
+    style: 'normal',
+    subsets: ['latin']
+})
+
+const inria_sans = Inria_Sans({
+    weight: '400',
     style: 'normal',
     subsets: ['latin']
 })
@@ -10,6 +17,7 @@ function Nav() {
     return(
         <div className="flex w-[81rem] h-[3rem] mx-[4.5rem]
          mt-6 justify-between items-center shrink-0">
+
             {/**Company logo. */}
             <div className="flex w-[7.8125rem] h-[1.5rem]
              justify-center items-center shrink-0">
@@ -18,6 +26,27 @@ function Nav() {
                     TaskGlitter
                 </h1>
             </div>
+
+            {/**Product marketing & sales links */}
+            <div className='flex w-[10.375rem] h-[1.375rem]
+             items-center gap-8 shrink-0'
+            >
+                <Link href={'#'}>
+                    <h1 className={`${inria_sans.className} text-[1.125rem] text-black leading-normal`
+                     }
+                    >
+                        Products
+                    </h1>
+                </Link>
+                <Link href={'#'}>
+                    <h1 className={`${inria_sans.className} text-[1.125rem] text-black leading-normal`
+                     }
+                    >
+                        Contacts
+                    </h1>
+                </Link>
+            </div>
+
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import prisma from "./lib/prisma"
 
-export const revalidate = 20;
 
 export default async function Home() {
   
-  const projects = await getProjects();
 
   return(
-    <div>
-        <h1 className="text-5xl text-blue-500">
-          Hello, World!
-        </h1>
-        {projects && projects.map((project, index) => (
-          <li key={index}>
-            {project.name}
-          </li>
-        ))}
-        
-    </div>
+    <>  
+    </>
     
   )
-}
-
-async function getProjects() {
-  const projects = await prisma.projects.findMany();
-  return projects;
 }
