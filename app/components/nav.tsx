7c895fbecad5828678e28a5fe27e585beba4b307
ratidzo <ratygore@gmail.com>
2023-09-12 11:45:53 +0200

    feat: setup logo in nav bar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,25 @@
+import {Josefin_Sans} from 'next/font/google'
+
+const josefin_sans = Josefin_Sans({
+    weight: '700',
+    style: 'normal',
+    subsets: ['latin']
+})
+
 function Nav() {
     return(
-        <>
-            
-        </>
+        <div className="flex w-[81rem] h-[3rem] mx-[4.5rem]
+         mt-6 justify-between items-center shrink-0">
+            {/**Company logo. */}
+            <div className="flex w-[7.8125rem] h-[1.5rem]
+             justify-center items-center shrink-0">
+                <h1 className={`${josefin_sans.className}
+                 text-2xl leading-normal`}>
+                    TaskGlitter
+                </h1>
+            </div>
+        </div>
     )
 }
+
+export {Nav};
