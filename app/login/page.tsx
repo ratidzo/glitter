9944b9f0c3184fd61a86e01fd9f88a392f31013b
ratidzo <ratygore@gmail.com>
2023-09-15 17:12:45 +0200

    feat: setup login
    Setup login section

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,28 @@
 function Login() {
     return(
-        <h1>Login Page</h1>
+        <div className="flex w-[68.91569rem] justify-between
+        items-center mt-[8.06rem] mb-[21.2rem]
+         ml-[4.5rem] mr-[16.6rem] ">
+
+            {/**Illustration */}
+            <>
+            </>
+
+            {/**Login Form */}
+
+            <div className="flex w-[25.75rem] h-[34.75rem]
+            py-[0.9375rem] flex-col justify-between items-center
+            shrink-0 ">
+
+                {/**Login to TaskGlitter */}
+                <div >
+
+                </div>
+            </div>
+
+        </div>
     )
 }
 
 export default Login;
+
