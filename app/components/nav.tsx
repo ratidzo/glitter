ef5b5233f627cd1523d3f25690670f559729fb39
ratidzo <ratygore@gmail.com>
2023-09-15 20:52:20 +0200

    feat: link logo to landing page
    Wrap logo with home link

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -26,13 +26,15 @@ function Nav() {
          mt-6 justify-between items-center shrink-0">
 
             {/**Company logo. */}
-            <div className="flex w-[7.8125rem] h-[1.5rem]
-             justify-center items-center shrink-0">
-                <h1 className={`${josefin_sans.className}
-                 text-2xl leading-normal`}>
-                    TaskGlitter
-                </h1>
-            </div>
+            <Link href={'/'}>
+                <div className="flex w-[7.8125rem] h-[1.5rem]
+                justify-center items-center shrink-0">
+                    <h1 className={`${josefin_sans.className}
+                    text-2xl leading-normal`}>
+                        TaskGlitter
+                    </h1>
+                </div>
+            </Link>
 
             {/**Product marketing & sales links */}
             <div className='flex w-[10.375rem] h-[1.375rem]
