b902e60bcbd67fb2a3ef7777a132920b356b546a
ratidzo <ratygore@gmail.com>
2023-09-15 18:20:22 +0200

    feat: resolve layout issue
    setup email label and field.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,50 +14,50 @@ function Login() {
          ml-[4.5rem] mr-[16.6rem] ">
 
             {/**Illustration */}
-            <>
-            </>
+            <div>
+            </div>
 
             {/**Login Form */}
-
-            <div className="flex flex-col w-[25.75rem] h-[34.75rem]
-            py-[0.9375rem] justify-between items-center
-            shrink-0 ">
+            
+            <form className="flex flex-col w-[25.75rem] h-[34.75rem]
+                py-[0.9375rem] items-center justify-between
+                shrink-0 ">
 
                 {/**Login to TaskGlitter */}
                 <div className="flex justify-center items-center
-                gap-[0.625rem] mb-8 ">
-                    <h1 className={`${inria_sans_bold.className}
-                     text-[2rem] leading-normal`}>
-                        Log in to TaskGlitter
-                    </h1>
-
-                    {/**Black section */}
-                    <div className="flex h-[2.375rem] justify-center
-                    items-center gap-[0.625rem] shrink-0 w-[17.6875rem]">
-                    </div>
-
-
-                    {/**Email Section */}
-                    <div className="flex h-[4.9375rem] flex-col
-                     items-start gap-2 shrink-0 ">
-                        {/** */}
-                        <label htmlFor="email" className={`
-                        ${inria_sans_bold.className} text-[1rem]
-                        leading-normal`}>
-                            Email
-                        </label>
-
-
-                    </div>
-
-                    
-
+                     gap-[0.625rem] mb-8 ">
+                        <h1 className={`${inria_sans_bold.className}
+                            text-[2rem] leading-normal flex`}>
+                            Log in to TaskGlitter
+                        </h1>
                 </div>
-            </div>
+
+                
+                {/**Email Section */}
+                <div className="flex h-[4.9375rem] flex-col
+                items-start gap-2 shrink-0 ">
+                    {/** */}
+                    <label htmlFor="email" className={`
+                    ${inria_sans_bold.className} text-[1rem]
+                    leading-normal`}>
+                        Email
+                    </label>
+                    {/**Email input */}
+                    <input name="email" type="text"
+                    className="w-[18.0625rem] h-[3rem] shrink-0
+                    ring-2 ring-black rounded "/>
+                </div>
+
+
+
+                
+            </form>
+
 
         </div>
     )
 }
 
 export default Login;
+
 
