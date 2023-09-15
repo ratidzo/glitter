5559bf569fe3430b98127333687fa8ffebf00855
ratidzo <ratygore@gmail.com>
2023-09-15 17:46:21 +0200

    feat: setup email section

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,23 +19,39 @@ function Login() {
 
             {/**Login Form */}
 
-            <div className="flex w-[25.75rem] h-[34.75rem]
-            py-[0.9375rem] flex-col justify-between items-center
+            <div className="flex flex-col w-[25.75rem] h-[34.75rem]
+            py-[0.9375rem] justify-between items-center
             shrink-0 ">
 
                 {/**Login to TaskGlitter */}
                 <div className="flex justify-center items-center
                 gap-[0.625rem] mb-8 ">
-                    <h1>
+                    <h1 className={`${inria_sans_bold.className}
+                     text-[2rem] leading-normal`}>
                         Log in to TaskGlitter
-
-                        color: #000;
-font-family: Inria Sans;
-font-size: 2rem;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
                     </h1>
+
+                    {/**Black section */}
+                    <div className="flex h-[2.375rem] justify-center
+                    items-center gap-[0.625rem] shrink-0 w-[17.6875rem]">
+                    </div>
+
+
+                    {/**Email Section */}
+                    <div className="flex h-[4.9375rem] flex-col
+                     items-start gap-2 shrink-0 ">
+                        {/** */}
+                        <label htmlFor="email" className={`
+                        ${inria_sans_bold.className} text-[1rem]
+                        leading-normal`}>
+                            Email
+                        </label>
+
+
+                    </div>
+
+                    
+
                 </div>
             </div>
 
